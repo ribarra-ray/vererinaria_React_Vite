8fe195c4e62e024e92bf04bd42df19281420e277
Raynier <raynieribarra@gmail.com>
2023-06-05 10:36:28 -0300

    Comentario del Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ function Header({isFunc}) {
 
     return (
         <div className="text-center uppercase">
-            <h2 className=" text-4xl ">Veterinaria</h2>
+            <h2 className=" text-4xl mr-2">Veterinaria</h2>
+            <h4>ConGithub</h4>
             <span className="text-sm">{isFunc("parametro")}</span>
            
         </div>
